9b873918ff23c393f32777a1af8212be67f76602
Angel Ferreira <angel.ferreira@casai.com>
2022-11-25 09:56:41 -0600

    Add funnels

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import snowflake.connector
 import config
 import streamlit as st
+import numpy as np
+import pandas as pd
+from pandas.api.types import CategoricalDtype
+from geopy.geocoders import Nominatim
 
 
 ctx = snowflake.connector.connect(
@@ -19,7 +23,16 @@ cur = ctx.cursor()
 @st.cache
 def load_event_data(event_id):
     # Execute a query to extract the data
-    sql = f"""select *
+    sql = f"""select
+                event_id,
+                name,
+                subcategory,
+                city,
+                state,
+                convert_timezone('UTC', 'America/Mexico_City', started_at) as started_at,
+                bookings_completed,
+                tickets_sold,
+                total_ticket_sales
             from EVENTS.EVENTS
             where event_id = {event_id}
             """
@@ -39,7 +52,7 @@ def load_similar_events(event_id, price_threshold):
                 from PROD.EVENTS.EVENTS
                 where event_id = {event_id}
                 )
-            select 
+            select
                 event_id,
                 name,
                 subcategory,
@@ -67,11 +80,39 @@ def load_similar_events(event_id, price_threshold):
 @st.cache
 def load_bookings_by_date(event_id):
     # Execute a query to extract the data
-    sql = f"""select date(convert_timezone('America/Mexico_City', paid_at)) as fecha, count(booking_id) as compras
-            from EVENTS.COMPLETED_BOOKINGS
-            where event_id in ({','.join(event_id)})
-            group by fecha
-            order by fecha
+    sql = f"""
+            select
+                timestampdiff(
+                    day,
+                    convert_timezone('America/Mexico_City', coalesce(e.activated_at, e.created_at)),
+                    convert_timezone('America/Mexico_City', cb.paid_at)
+                    ) as dias_a_la_venta,
+                count(*) as compras
+            from PROD.EVENTS.COMPLETED_BOOKINGS cb
+            left join prod.events.events e on e.event_id = cb.event_id
+            where cb.event_id in ({','.join(event_id)})
+            and cb.paid_at > coalesce(e.activated_at, e.created_at)
+            group by 1
+            order by 1
+            """
+    cur.execute(sql)
+    # Converting data into a dataframe
+    df = cur.fetch_pandas_all()
+    return df
+
+@st.cache
+def load_bookings_by_payment_method(event_id):
+    # Execute a query to extract the data
+    sql = f"""
+            select
+                payment_method,
+                count(*) as compras
+            from PROD.EVENTS.COMPLETED_BOOKINGS cb
+            left join prod.events.events e on e.event_id = cb.event_id
+            where cb.event_id in ({','.join(event_id)})
+            and cb.paid_at > coalesce(e.activated_at, e.created_at)
+            group by 1
+            order by 1
             """
     cur.execute(sql)
     # Converting data into a dataframe
@@ -120,6 +161,82 @@ def load_bookings_by_week_day(event_id):
                 where event_id in ({','.join(event_id)})
                 group by dia
                 order by total_bookings
+                """
+    cur.execute(sql)
+    # Converting data into a dataframe
+    df = cur.fetch_pandas_all()
+    return df
+
+
+# Function to get the coordinates (lat and long) of each city
+@st.cache
+def get_coordinates(df):
+    geolocator = Nominatim(user_agent='Boletia_GA')
+    df['LAT'] = np.nan
+    df['LON'] = np.nan
+    for i in df.index:
+        location = geolocator.geocode(df['CITY'][i])
+        df['LAT'][i] = location.latitude
+        df['LON'][i] = location.longitude
+    return df
+
+
+# Function to get the events pageviews
+@st.cache
+def load_pageviews(event_id):
+    # Execute a query to extract the data
+    sql = f"""select
+                case
+                    when PAGE_PATH like '%/finish' then 'Pago'
+                    when PAGE_PATH like '%/pay' then 'Checkout'
+                    when PAGE_PATH like '%/info' then 'Info'
+                    else 'Inicio'
+                end as PAGE_PATH,
+                SOURCE_MEDIUM,
+                PAGEVIEWS
+                from EVENTS.SALES_FUNNELS
+                where SUBDOMAIN =
+                    (select subdomain
+                    from EVENTS.EVENTS
+                    where EVENT_ID = {event_id})
+                and PAGE_PATH in (
+                                  SUBDOMAIN || '.boletia.com/',
+                                  SUBDOMAIN || '.boletia.com/info',
+                                  SUBDOMAIN || '.boletia.com/pay',
+                                  SUBDOMAIN || '.boletia.com/finish')
+                order by PAGEVIEWS desc
+                    """
+    cur.execute(sql)
+    # Converting data into a dataframe
+    df = cur.fetch_pandas_all()
+    return df
+
+
+# Function to pivot the data and process the table
+@st.cache
+def get_funnel(df):
+    stages = CategoricalDtype(["Inicio", "Info", "Checkout", "Pago"], ordered=True)
+    df["PAGE_PATH"] = df["PAGE_PATH"].astype(stages)
+    df.sort_values('PAGE_PATH')
+    # pivot the source_medium values into columns
+    df = df.pivot_table('PAGEVIEWS', 'PAGE_PATH', 'SOURCE_MEDIUM')
+    # replacing nan values to zeros
+    df = df.fillna(0)
+    df = df.T
+    df['Tasa de conversión'] = df.apply(lambda x: f"{round(x['Pago'] / x['Inicio'] * 100,2)}%", axis = 1)
+    df = df.astype('str')
+    df = df.T
+    return df
+
+
+# Function to get the total number of bookings by city
+@st.cache(allow_output_mutation=True)
+def load_bookings_by_city(event_id):
+    # Execute a query to extract the data
+    sql = f"""select *
+                from EVENTS.CUSTOMER_DEMOGRAPHICS_CITY
+                where event_id = {event_id}
+                order by TOTAL_BOOKINGS DESC
                 """
     cur.execute(sql)
     # Converting data into a dataframe
